ee62200b6899378a18c6a4dc75673320e9b65e82
Sophie <sophiemcrouse@gmail.com>
2021-07-05 12:42:54 -0400

    temp fix, no auth

--- index.js
+++ index.js
@@ -9,35 +9,32 @@ const client = new SecretManagerServiceClient();
 const projectId = process.env.GCLOUD_PROJECT;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 //const authToken = process.env.TWILIO_AUTH_TOKEN;
+const name = 'projects/499116149825/secrets/twilio_auth_token/versions/1';
 const region = 'us-central1';
-
-exports.helloWorld = (req, res) => {
-    res.send('Hello World');
-};
 
 exports.reply = (req, res) => {
     // connect to the secret manager
-    let version = undefined | String;
-    function accessSecretVersion() {
-        const [version] = client.accessSecretVersion({
-            name: 'projects/499116149825/secrets/twilio_auth_token/versions/1'
-        });
-        const payload = version.payload.data.toString();
-    }
-    accessSecretVersion();
+    //let version = undefined | String;
+    // async function accessSecretVersion() {
+    //     const [version] = await client.accessSecretVersion({
+    //         name: name,
+    //     });
+    //     // extract payload as string
+    //     const payload = version.payload.data.toString();
+    // }
+    // accessSecretVersion();
 
     let isValid = true;
-    // Only validate that requests came from Twilio when the function has been
-    // deployed to production.
-    if (process.env.NODE_ENV === 'production') {
-        isValid = twilio.validateRequest(
-            //process.env.TWILIO_AUTH_TOKEN,
-            payload,
-            req.headers['x-twilio-signature'],
-            `https://${region}-${projectId}.cloudfunctions.net/reply`,
-            req.body
-        );
-    }
+    // // Only validate that requests came from Twilio when the function has been
+    // // deployed to production.
+    // if (process.env.NODE_ENV === 'production') {
+    //     isValid = twilio.validateRequest(
+    //         payload,
+    //         req.headers['x-twilio-signature'],
+    //         `https://${region}-${projectId}.cloudfunctions.net/reply`,
+    //         req.body
+    //     );
+    // }
 
     // Stop early if the request was not sent from Twilio
     // TODO: add logging
@@ -49,7 +46,6 @@ exports.reply = (req, res) => {
             .end();
         return;
     }
-
 
     const response = new MessagingResponse();
     response.message('Thanks for using You Never Walk Alone. ' +
