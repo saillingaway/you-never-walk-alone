7a350a05922370b4422793595bb64640c2955f01
Sophie <sophiemcrouse@gmail.com>
2021-07-07 00:31:41 -0400

    removed comments

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const name = 'projects/499116149825/secrets/twilio_auth_token/versions/1';
 const region = 'us-central1';
 let photo = "../mocks/taehyung_img.JPG";
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const memberAliases = require('./memberAliasMap.json');
 let memberData = require('./memberData.json');
@@ -28,37 +26,6 @@ function matchMember(memberName) {
     name = memberAliases[memberName]
     return name;
 }
-
-// let memberData = {
-//     "namjoon": {
-//         "quote": "Life is a sculpture that you cast as you make mistakes and learn from them.",
-//         "img": "https://storage.googleapis.com/you-never-walk-alone/ynwa-BTS/BTS_Namjoon/heart.JPG"
-//     },
-//     "seokjin": {
-//         "quote": "You worked hard. Keep trying your best.",
-//         "img": "https://storage.googleapis.com/you-never-walk-alone/ynwa-BTS/BTS_Seokjin/smile.JPG"
-//     },
-//     "yoongi": {
-//         "quote": "Those who don’t have a dream, it’s okay, it’s okay if you don’t have a dream. You just have to be happy.",
-//         "img": "https://storage.googleapis.com/you-never-walk-alone/ynwa-BTS/BTS_Yoongi/happyYoongi.JPG"
-//     },
-//     "hoseok": {
-//         "quote": "Don't ever make decisions based on fear. Make decisions based on hope and possibility",
-//         "img": "https://storage.googleapis.com/you-never-walk-alone/ynwa-BTS/BTS_Hoseok/stars.jpeg"
-//     },
-//     "jimin": {
-//         "quote": "Remember there is a person here in Korea, in the city of Seoul, who understands you.",
-//         "img": "https://storage.googleapis.com/you-never-walk-alone/ynwa-BTS/BTS_Jimin/pinkHeart.JPG"
-//     },
-//     "taehyung": {
-//         "quote": "Don’t be trapped in someone else’s dream.",
-//         "img": "https://storage.googleapis.com/you-never-walk-alone/ynwa-BTS/BTS_Taehyung/MonstersINC.JPG"
-//     },
-//     "jungkook": {
-//         "quote": "Without anger or sadness, you won’t be able to feel true happiness.",
-//         "img": "https://storage.googleapis.com/you-never-walk-alone/ynwa-BTS/BTS_Jungkook/pillow.JPG"
-//     }
-// }
 
 exports.send_boys = (req, res) => {
     let name;
